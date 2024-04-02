32a096041e968ffb5a9c0024e8e4a642674fb82a
NetflixClone <hosiduc2002.chem@gmail.com>
2024-04-02 21:43:34 +0700

    feat: Remove grid container and left pane

--- src/components/jobalerts/index.js
+++ src/components/jobalerts/index.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
 
-import DesktopNav from "./components/DesktopNav";
 import JobAlert from "./components/JobAlert";
 import MobileNav from "./components/MobileNav";
 import UnComfirmAlert from "./components/UnComfirmAlert";
@@ -23,31 +22,26 @@ function JobsAlerts() {
     setIsConfirmEmail(value);
   };
   return (
-    <div className="my-account-page content-container -width-xl grid-container -three-columns">
-      {/* desktop */}
-      <DesktopNav />
-      {/* content */}
-      <div className="my-account-content grid-content">
-        <h3 className="heading-large account-page-heading">Thông báo việc</h3>
-        {/* mobile */}
-        <MobileNav />
-        {!isConfirmEmail && (
-          <UnComfirmAlert uncomfirmHandler={uncomfirmHandler} />
-        )}
-        {/* alert */}
-        {isConfirmEmail && (
-          <>
-            <div id="email-alerts">
-              {JobAlertData.map((item) => (
-                <JobAlert key={item.id} title={item.title} />
-              ))}
-            </div>
-            <button type="button" className="create-alert-button -primary">
-              Tạo thông báo việc làm ngay
-            </button>
-          </>
-        )}
-      </div>
+    <div className="my-account-content grid-content">
+      <h3 className="heading-large account-page-heading">Thông báo việc</h3>
+      {/* mobile */}
+      <MobileNav />
+      {!isConfirmEmail && (
+        <UnComfirmAlert uncomfirmHandler={uncomfirmHandler} />
+      )}
+      {/* alert */}
+      {isConfirmEmail && (
+        <>
+          <div id="email-alerts">
+            {JobAlertData.map((item) => (
+              <JobAlert key={item.id} title={item.title} />
+            ))}
+          </div>
+          <button type="button" className="create-alert-button -primary">
+            Tạo thông báo việc làm ngay
+          </button>
+        </>
+      )}
     </div>
   );
 }
