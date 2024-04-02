2f83379bd2be56e98648d0457b4c1acb0f153a1a
20120392 <ptbt1092@gmail.com>
2024-04-03 00:16:12 +0700

    update login page

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,29 +1,12 @@
-/* eslint-disable no-unused-vars */
 /* eslint-disable react/button-has-type */
-/* eslint-disable jsx-a11y/anchor-is-valid */
-/* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useEffect, useRef, useState } from "react";
-import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
 
 import CandidateLogin from "../../components/login/candidate-login";
-import { loginAccount, selectUser } from "../../store/user";
 
 import "../../components/login/login-style.css";
 
 function Login({ onClose = () => {}, show = false }) {
-  const dispatch = useDispatch();
   const loginRef = useRef();
-  const navigate = useNavigate();
-
-  const [isLoginVisible, setIsLoginVisible] = useState(false);
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Handle the form submission
-    // Test with static email
-    const userEmail = "user@example.com";
-    dispatch(loginAccount({ email: userEmail }));
-  };
 
   const [shouldAddListener, setShouldAddListener] = useState(
     window.innerWidth >= 768
@@ -31,7 +14,10 @@ function Login({ onClose = () => {}, show = false }) {
 
   const handleRoleSelection = (role) => {
     sessionStorage.setItem("role", role);
-    setIsLoginVisible(role === "candidate"); // Show the login form if role is candidate
+    if (role === "candidate") {
+      document.querySelector(".role-selection-container").style.display =
+        "none";
+    }
   };
 
   const handleClickOutside = (event) => {
@@ -40,14 +26,11 @@ function Login({ onClose = () => {}, show = false }) {
     }
   };
 
-  const handleCreateAccountClick = () => {
-    navigate("/register"); // Navigate to the Register page
-  };
-
   useEffect(() => {
     const role = sessionStorage.getItem("role");
     if (role === "candidate") {
-      setIsLoginVisible(true);
+      document.querySelector(".role-selection-container").style.display =
+        "none";
     }
 
     const updateListener = () => {
@@ -69,26 +52,26 @@ function Login({ onClose = () => {}, show = false }) {
 
   return (
     <div className={`login-widget ${show ? "active" : ""}`} ref={loginRef}>
-      {isLoginVisible ? (
-        <CandidateLogin onClose={onClose} />
-      ) : (
-        <div className="role-selection-container">
-          <h3 className="login-title">Đăng nhập</h3>
-          <p className="mb-24">Bạn là ứng viên tìm việc hay nhà tuyển dụng?</p>
-          <button
-            className="rounded-button-primary btn-sign-up mb-24"
-            onClick={() => handleRoleSelection("candidate")}
-          >
-            Ứng Viên
-          </button>
-          <button
-            className="rounded-button-primary btn-forgot-pw"
-            onClick={() => handleRoleSelection("employer")}
-          >
-            Nhà Tuyển Dụng
-          </button>
-        </div>
-      )}
+      <div
+        className="role-selection-container"
+        style={{ position: "absolute", display: "block" }}
+      >
+        <h3 className="login-title">Đăng nhập</h3>
+        <p className="mb-24">Bạn là ứng viên tìm việc hay nhà tuyển dụng?</p>
+        <button
+          className="rounded-button-primary btn-sign-up mb-24"
+          onClick={() => handleRoleSelection("candidate")}
+        >
+          Ứng Viên
+        </button>
+        <button
+          className="rounded-button-primary btn-forgot-pw"
+          onClick={() => handleRoleSelection("employer")}
+        >
+          Nhà Tuyển Dụng
+        </button>
+      </div>
+      <CandidateLogin onClose={onClose} />
     </div>
   );
 }
