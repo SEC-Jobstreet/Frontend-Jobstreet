f403f6a7b3c50d74a5eb7d9edc64809f1fcae104
Tho - hcmus <96709107+Sekiro2002@users.noreply.github.com>
2024-04-03 10:29:32 +0700

    fix search-form

--- src/components/searchform/index.js
+++ src/components/searchform/index.js
@@ -1,5 +1,9 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 // Import any necessary libraries or modules
 import React from "react";
+
+import LocationPinIcon from "../icon/location-icon";
+import SearchIcon from "../icon/search-icon";
 
 import "./searchform.css";
 // Define your SearchForm component
@@ -9,26 +13,37 @@ function SearchForm() {
   return (
     <div className="search-form">
       <div className="keyword-input search-input-field">
-        <span htmlFor="q"> Từ khóa </span>
-        <input
-          id="q"
-          maxLength="512"
-          name="q"
-          placeholder="Tên việc, công ty, từ khóa"
-          type="search"
-          autoComplete="off"
-        />
+        <label htmlFor="q"> Từ khóa </label>
+        <div className="input-field">
+          <input
+            id="q"
+            maxLength="512"
+            name="q"
+            placeholder="Tên việc, công ty, từ khóa"
+            type="search"
+            autoComplete="off"
+          />
+          <div className="ti-search">
+            <SearchIcon />
+          </div>
+        </div>
       </div>
       <div className="location-input search-input-field">
-        <span htmlFor="l"> Địa điểm </span>
-        <input
-          id="l"
-          maxLength="512"
-          name="l"
-          placeholder="Thành phố, quận, huyện"
-          type="search"
-          autoComplete="off"
-        />
+        <label htmlFor="l"> Địa điểm </label>
+        <div className="input-field">
+          <input
+            id="l"
+            maxLength="512"
+            name="l"
+            placeholder="Thành phố, quận, huyện"
+            type="search"
+            autoComplete="off"
+            icon="location"
+          />
+          <div className="ti-search">
+            <LocationPinIcon />
+          </div>
+        </div>
       </div>
       <div className="search-button">
         <button type="submit" className="search-job-button">
