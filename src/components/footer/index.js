2838e0ea660497447f13e92a7bbfd34e62797e03
Gia Hao <jzahao@gmail.com>
2024-04-03 13:32:53 +0700

    create footer

---
+++ src/components/footer/index.js
@@ -0,0 +1,31 @@
+import "./footer.css";
+
+function Footer() {
+  return (
+    <footer>
+      <div className="footer">
+        <a className="footer-link" href="/">
+          Tìm việc làm
+        </a>
+        <a className="footer-link" href="/">
+          Danh mục công ty
+        </a>
+        <a className="footer-link" href="/">
+          Tìm kiếm phổ biến
+        </a>
+        <a className="footer-link" href="/">
+          Giới thiệu
+        </a>
+        <a className="footer-link" href="/">
+          Câu hỏi thường gặp
+        </a>
+        <a className="footer-link" href="/">
+          Những thay đổi mới
+        </a>
+        <span className="copyright-text">©2024 Job Seeker Pty Ltd</span>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
