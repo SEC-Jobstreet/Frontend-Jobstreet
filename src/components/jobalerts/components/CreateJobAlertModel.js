60fdfe2724f871697a1c33833e7d12bf9718f5a5
NetflixClone <hosiduc2002.chem@gmail.com>
2024-04-10 10:32:42 +0700

    fix: Fix border input form, handler clear text input

--- src/components/jobalerts/components/CreateJobAlertModel.js
+++ src/components/jobalerts/components/CreateJobAlertModel.js
@@ -1,17 +1,46 @@
-import { Button, Form } from "react-bootstrap";
+import { useState } from "react";
+import { Button, Form, InputGroup } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 
 import Info from "../../../assets/svg/circle_info_icon.svg";
+import Dismiss from "../../../assets/svg/dismiss_icon.svg";
 import Email from "../../../assets/svg/email_icon.svg";
+import Location from "../../../assets/svg/location_icon.svg";
+import Search from "../../../assets/svg/search_icon.svg";
 import Model from "../model/Model";
+
+import "../index.css";
 
 export default function CreateJobAlertModel(props) {
   const { onHideModelHandler } = props;
+  const [inputKeyWords, setInputKeyWords] = useState("");
+  const [inputLocation, setInputLocation] = useState("");
+
+  const onInputChange = (event, identifier) => {
+    const enteredValue = event.target.value;
+
+    if (identifier === "key-word") {
+      setInputKeyWords(enteredValue);
+    }
+    if (identifier === "location") {
+      setInputLocation(enteredValue);
+    }
+  };
+  const onClearInputHandler = (identifier) => {
+    if (identifier === "key-word") {
+      setInputKeyWords("");
+    }
+
+    if (identifier === "location") {
+      setInputLocation("");
+    }
+  };
   return (
     <Model onHideModelHandler={onHideModelHandler}>
       <div
         className="content-container -width-md -margin-y-md"
         id="email-alert-management-page"
+        style={{ width: "100%", overflowY: "auto", margin: "0px" }}
       >
         <h2 className="heading-large">
           <img src={Email} alt="email-icon" style={{ marginRight: "0.8rem" }} />
@@ -25,12 +54,29 @@ export default function CreateJobAlertModel(props) {
           {/* từ khoá */}
           <Form.Group className="email-alert-container">
             <Form.Label htmlFor="email-alert-query">Từ khoá</Form.Label>
-            <Form.Control
-              className="email-alert"
-              placeholder="Tên việc, công ty, từ khóa"
-              type="text"
-              id="email-alert-query"
-            />
+            <InputGroup className="email-alert">
+              <Form.Control
+                className="email-alert"
+                placeholder="Tên việc, công ty, từ khóa"
+                type="text"
+                id="email-alert-query"
+                value={inputKeyWords}
+                onChange={(event) => onInputChange(event, "key-word")}
+              />
+              <InputGroup.Text id="dismiss">
+                {inputKeyWords !== "" && (
+                  <img
+                    src={Dismiss}
+                    alt="location icon"
+                    onClick={() => onClearInputHandler("key-word")}
+                    aria-hidden="true"
+                  />
+                )}
+              </InputGroup.Text>
+              <InputGroup.Text>
+                <img src={Search} alt="search icon" />
+              </InputGroup.Text>
+            </InputGroup>
             <div className="field-description">
               <img
                 src={Info}
@@ -51,13 +97,29 @@ export default function CreateJobAlertModel(props) {
           {/* địa điểm */}
           <Form.Group className="email-alert-container">
             <Form.Label htmlFor="email-alert-location">Địa điểm</Form.Label>
-            <Form.Control
-              className="email-alert"
-              placeholder="Thành phố, quận, huyện"
-              type="text"
-              id="email-alert-location"
-            />
-
+            <InputGroup className="email-alert">
+              <Form.Control
+                className="email-alert"
+                placeholder="Thành phố, quận, huyện"
+                type="text"
+                id="email-alert-location"
+                value={inputLocation}
+                onChange={(event) => onInputChange(event, "location")}
+              />
+              <InputGroup.Text id="dismiss">
+                {inputLocation !== "" && (
+                  <img
+                    src={Dismiss}
+                    alt="location icon"
+                    onClick={() => onClearInputHandler("location")}
+                    aria-hidden="true"
+                  />
+                )}
+              </InputGroup.Text>
+              <InputGroup.Text>
+                <img src={Location} alt="location icon" />
+              </InputGroup.Text>
+            </InputGroup>
             <div className="field-description">
               <img
                 src={Info}
